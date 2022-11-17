46cd96b43c947d073da7c071df892d2f53c19862
Relaroman48RUS <realroman444@yandex.ru>
2022-11-17 19:40:32 +0300

    github234

--- first_prg.py
+++ first_prg.py
@@ -1,7 +1,7 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('And I change it every day efdsgqdwqvdf')
+    print('And I change it every day efdsgqdwq1vdf')
 
 
 if __name__ == '__main__':
